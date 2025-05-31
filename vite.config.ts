ef4b61f02af1d110f585ceec5da87ff49cf49c4c
Asad Mahmood <121114779+asadtechlead@users.noreply.github.com>
2025-05-31 16:29:34 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/precise-price-print/' : '/',
   server: {
     host: "::",
     port: 8080,
